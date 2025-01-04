26004e74fecc6e8a8642238a80ae436580df865c
Tamim Hossain <132823494+CodeWithTamim@users.noreply.github.com>
2025-01-04 11:42:50 +0600

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = packageName
         minSdk = 21
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2-beta"
+        versionCode = 4
+        versionName = "1.0.3-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
